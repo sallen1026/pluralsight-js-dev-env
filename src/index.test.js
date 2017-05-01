3544c9a0fa861950ffcd74238b59c2200be902ef
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2017-04-30 21:33:09 -0400

    Ch 9 : Test and CI changes

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say hello', () => {
+        const index = fs.readFileSybc('./src/index.html', "utf-8");
+        jsdom.env(index,)
+    })
+})

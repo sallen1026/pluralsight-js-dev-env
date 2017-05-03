eb99580e10204763871242e9c2a93a86826727af
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2017-05-02 21:22:28 -0400

    Centralized userAPI

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-    it('should say hello', (done) => {                                  // "it" takes a parameter that tells it when the asynchronous call is "done"
+    it('should have h1 that says Users', (done) => {                                  // "it" takes a parameter that tells it when the asynchronous call is "done"
         const index = fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function(err, window){                         // this is an asynchronous callback function, so you have to tell it when it's done
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello World!");
+            expect(h1.innerHTML).to.equal("Users");
             done();                                                     // this tells Mocha that our test is "done", and it will then run the expect to say whether it's true or false
             window.close();
         });

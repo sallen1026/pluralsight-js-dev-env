9d61a672e9011ff5d2c1355daeeaa53e07acf12b
ChiliDogDaddyO <chilidogdaddyo@gmail.com>
2017-05-05 09:14:03 -0400

    Added env variable and nice messages and stats to build script

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -4,6 +4,9 @@ import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
 
+// Babel (and other tools) look for this environment variable to know how to build the output
+process.env.NODE_ENV = 'production';
+
 webpack(webpackConfig).run((err, stats) => {
     if (err) { // so a fatal error occurred; stop here.
         console.log(chalk.red(err));

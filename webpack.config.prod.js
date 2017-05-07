eca9652c9c98b0d2c7175a23ef50a1e5a16b6b0b
Steve Allen <ChiliDogDaddyO@gmail.com>
2017-05-07 10:27:18 -0400

    Ch 12 - Extract and minifying CSS

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';  // we need to add this import since we're now calling some webpack specific features
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -17,14 +19,21 @@ export default {
     // here we changed out output from src to dist
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+      // Generate an external css file with a hash in the filename
+      new ExtractTextPlugin('[name].[contenthash].css'),
+
+      // Hash the files using MD5 so that their names change when their content changes
+      new WebpackMd5Hash(),
+
       // Use CommonsChunkPlugin to create a separate bundle
       // of vendor libraries so that they're cached separately
       new webpack.optimize.CommonsChunkPlugin({
           name: 'vendor'
       }),
+
       // Create HTML file that includes reference to bundled JS
       new HtmlWebpackPlugin({
           template: 'src/index.html',
@@ -42,15 +51,17 @@ export default {
           },
           inject: true
       }),
+
       // Eliminate duplicate packages when generating bundle
       new webpack.optimize.DedupePlugin(),
+
       // Minify JS
       new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
